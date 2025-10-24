4e585417901888284eed8fac5440dac5a6c755da
Sacha Bertaina--David <86232952+dice1-nat20@users.noreply.github.com>
2025-10-24 11:39:02 +0200

    Fonctions de t_cell.c et corrections dans t_cell.h

--- t_cell.h
+++ t_cell.h
@@ -16,12 +16,25 @@ typedef struct t_cell {
 
 typedef struct t_list {
     t_cell *head;
-    t_cell arretes[MAX_SIZE];
 }t_list;
 
 typedef struct list_adjac {
     int taille;
     t_list *adjac_sommets;
 }list_adjac;
+
+
+t_cell* createCell(int sommet,float proba);
+
+t_list createEmptyList();
+
+void addCellHead(int sommet,float proba,t_list* l);
+
+void displayList(t_list* l);
+
+void displayListAdjac(list_adjac* la);
+
+list_adjac* createListAdjac(int taille);
+
 
 #endif //T_CELL_H
