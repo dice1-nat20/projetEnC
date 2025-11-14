452a3b5f942a5a2c334d527282e464891a248557
manon <manon.souchon--garrigue@efrei.net>
2025-11-14 17:18:53 +0100

    struct tarjan

---
+++ tarjan.h
@@ -0,0 +1,15 @@
+//
+// Created by manon on 14/11/2025.
+//
+
+#ifndef TARJAN_H
+#define TARJAN_H
+
+typedef struct {
+    int identifiant;
+    int numeroTemp;
+    int numAcces;
+    int boolTraitement;
+}d_tergent_vertex;
+
+#endif //TARJAN_H
