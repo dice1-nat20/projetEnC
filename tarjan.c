3a078acf46967efcc8ffc608211bb4614533b7b2
manon <manon.souchon--garrigue@efrei.net>
2025-11-17 10:58:02 +0100

    struct tarjan

--- tarjan.c
+++ tarjan.c
@@ -4,15 +4,15 @@
 
 #include "tarjan.h"
 
-l_tergent create_l_tarjan(list_adjac graphe) {
-    int nombre=graphe.taille;
-    l_tergent *liste_tergent = (l_tergent*) malloc(sizeof(l_tergent));
-    for (int i=0;i<=nombre;i++) {
-        liste_tergent[i]->identifiant=graphe.adjac_sommets->head->sommet;
-        graphe.adjac_sommets->head=graphe.adjac_sommets->head->next;
-        liste_tergent[i]->numeroTemp=-1;
-        liste_tergent[i]->numAcces=-1;
-        liste_tergent[i]->boolTraitement=0;
-    }
-    return *liste_tergent;
-}
+// l_tergent create_l_tarjan(list_adjac graphe) {
+//     int nombre=graphe.taille;
+//     l_tergent *liste_tergent = (l_tergent*) malloc(sizeof(l_tergent));
+//     for (int i=0;i<=nombre;i++) {
+//         liste_tergent[i]->identifiant=graphe.adjac_sommets->head->sommet;
+//         graphe.adjac_sommets->head=graphe.adjac_sommets->head->next;
+//         liste_tergent[i]->numeroTemp=-1;
+//         liste_tergent[i]->numAcces=-1;
+//         liste_tergent[i]->boolTraitement=0;
+//     }
+//     return *liste_tergent;
+// }
--- tarjan.h
+++ tarjan.h
@@ -5,19 +5,19 @@
 #ifndef TARJAN_H
 #define TARJAN_H
 #include "t_cell.h"
-typedef struct {
-    int identifiant;
-    int numeroTemp;
-    int numAcces;
-    int boolTraitement;
-}d_tergent_vertex,*l_tergent;
-
-typedef struct {
-    char name[4];
-    l_tergent ensemble;
-}t_classe,*l_classe;
-
-l_tergent create_l_tarjan(list_adjac);
+// typedef struct {
+//     int identifiant;
+//     int numeroTemp;
+//     int numAcces;
+//     int boolTraitement;
+// }d_tergent_vertex,*l_tergent;
+//
+// typedef struct {
+//     char name[4];
+//     l_tergent ensemble;
+// }t_classe,*l_classe;
+//
+// l_tergent create_l_tarjan(list_adjac);
 
 
 
