b024d30ff9a717ed9cf8715f69f6c666e1a6da99
manon <manon.souchon--garrigue@efrei.net>
2025-11-17 10:53:18 +0100

    struct tarjan

--- tarjan.h
+++ tarjan.h
@@ -4,12 +4,21 @@
 
 #ifndef TARJAN_H
 #define TARJAN_H
-
+#include "t_cell.h"
 typedef struct {
     int identifiant;
     int numeroTemp;
     int numAcces;
     int boolTraitement;
-}d_tergent_vertex;
+}d_tergent_vertex,*l_tergent;
+
+typedef struct {
+    char name[4];
+    l_tergent ensemble;
+}t_classe,*l_classe;
+
+l_tergent create_l_tarjan(list_adjac);
+
+
 
 #endif //TARJAN_H
