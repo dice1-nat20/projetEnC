88e6ad1053b9a9fd89b060a1d7ded7d6bb9e3127
ulysse <ulysse.clz33@gmail.com>
2025-11-20 09:26:06 +0100

    Partie 2 : affichage partition et hasse, nouveau test

--- main.c
+++ main.c
@@ -2,14 +2,17 @@
 #include "lecture.h"
 #include "utils.h"
 #include "hasse.h"
+#include "utils.h"
 
 int main() {
-    list_adjac * list1 = readGraph("data/exemple1_chatGPT_fixed.txt");
+    list_adjac * list1 = readGraph("data/exemple_custom.txt");
     displayListAdjac(list1);
+    CreateMermaidGraph(list1);
     verifMarkovGraph(list1);
     t_partition* graphTarjan = tarjan(list1);
     displayPartition(graphTarjan);
-    hasse(graphTarjan,list1);
+    t_hasse * graphHasse = hasse(graphTarjan,list1);
+    CreateMermaidPartition(graphTarjan, graphHasse);
 
     return 0;
 
